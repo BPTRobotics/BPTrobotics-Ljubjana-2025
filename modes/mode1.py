27f68c3794f36899b0c0b8b3ebc46d2dae61c628
Márkus Péter <24d_markusp@nyirszikszi.hu>
2025-08-21 15:50:48 +0200

    Added lidar precision.

--- modes/mode1.py
+++ modes/mode1.py
@@ -3,16 +3,11 @@ from ..control import motor
 from ..sensors import lidar
 from time import sleep
 from ..sensors.gyroscope import INITIAL_DIRECTION
-
-msg = None
-def callback(__msg):
-    global msg
-    msg = __msg
-lidar.setup.init(callback)
+from . import lidar_manager
 
 
-def start(DISTANCE_ = .7):
-    global msg
+def start(DISTANCE_ = 1.1):
+    global lidar_manager
     motor.set_speed(1)
     distance = 0
 
@@ -23,7 +18,7 @@ def start(DISTANCE_ = .7):
 
         if pitch_diff and INITIAL_DIRECTION.value: print(f"MODE (1): Pitch difference: {pitch_diff:.2f} INITIAL_DIRECTION: {INITIAL_DIRECTION.value:.2f}")
 
-        if msg: distance = lidar.utils.extract_distance(180,msg)
+        if lidar_manager.msg: distance = lidar.utils.extract_distance(180,lidar_manager.msg)
         else: 
             motor.stop()
             continue
